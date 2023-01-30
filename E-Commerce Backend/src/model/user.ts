39d6d5f1a27de457b989b80f6037100df0929f44
Salah Ashraf <salahashraf924@gmail.com>
2023-01-30 22:49:55 +0200

    create a model for user

---
+++ E-Commerce Backend/src/model/user.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+const Schema=mongoose.Schema;
+const model=mongoose.model;
+
+const userSchema=new Schema({
+    id:{
+        type:String,
+        required:true
+    },
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 10,
+        max: 30,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024,
+    },
+    accountType:{
+        type:String,
+        requried:true,
+        min:5,
+        max:6
+    },
+    phoneNumber:{
+        type:String,
+        required:true,
+        length:11
+    }
+});
+
+const User= mongoose.model("User", userSchema);
+export default User;
