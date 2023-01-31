842cb2df8e8ae0a6f13214ad89bb134765e73d28
Salah Ashraf <salahashraf924@gmail.com>
2023-01-31 14:17:59 +0200

    fix: route of get products

--- E-Commerce Backend/src/routes/ProductRoutes.ts
+++ E-Commerce Backend/src/routes/ProductRoutes.ts
@@ -3,5 +3,5 @@ import { createProduct, getProducts } from "../controller/ProductController";
 const router = express.Router();
 
 router.post("/products", createProduct);
-router.get("/products/?category=<>&orderBy=<>&sellerId=<>", getProducts);
+router.get("/products", getProducts);
 export default router;
