2e1157b70ed745f979cf243ed6ae213f16213370
Salah Ashraf <salahashraf924@gmail.com>
2023-01-31 14:29:39 +0200

    fix: Replace Magic Number with Symbolic Constant

--- E-Commerce Backend/src/controller/UserController.ts
+++ E-Commerce Backend/src/controller/UserController.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 const User=require('../model/user').User;
 import {encryptPassword,generateRandomID}from '../HelperFunctions/index';
+import statusCode from '../constants/StatusCode';
 
-export async function createUser(request: express.Request, response: express.Response, next: express.NextFunction) {
+export async function createUser(request: express.Request, response: express.Response) {
     let userToCreate=new User(request.body);
     userToCreate.id=generateRandomID();
     userToCreate.password= await encryptPassword(userToCreate.password);
     userToCreate=await User.create(userToCreate);
-    return response.status(201).json(`Signed Up. ${userToCreate}`);
+    return response.status(statusCode.CREATED).json(`Signed Up. ${userToCreate}`);
 }
-export async function updateUser(request: any, response: express.Response, next: express.NextFunction){
+export async function updateUser(request: any, response: express.Response){
     let userId=request.params.userId;
     let userToUpdate=({
         name:request.body.name,
@@ -25,7 +26,7 @@ export async function updateUser(request: any, response: express.Response, next:
         console.log("Error at update user"+err);
     });
 }
-export async function deleteUser(request: any, response: express.Response, next: express.NextFunction){
+export async function deleteUser(request: any, response: express.Response){
     let userId=request.params.userId;
     const filter ={id:userId};
     if(await User.exists(filter)){
