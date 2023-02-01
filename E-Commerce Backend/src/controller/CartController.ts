6a228f0ef102ee6661744810fc1121641ebb0168
Salah Ashraf <salahashraf924@gmail.com>
2023-02-01 17:23:04 +0200

    optimize the code & fix quantity bug

--- E-Commerce Backend/src/controller/CartController.ts
+++ E-Commerce Backend/src/controller/CartController.ts
@@ -8,31 +8,35 @@ const { ObjectID, default: BSON } = require("bson");
 export async function addProduct(request: express.Request, response: express.Response) {
     const userId=request.params.userId;
     const productId=request.params.productId;
-    let productToAdd= await Product.findOne({"_id":(productId)});
-    if(!productToAdd){
-        productToAdd=new Product();
-    }
-    const quantity:any=productToAdd.quantity;
+    const productFilter={"_id":productId};
+    let productToAdd:any= await Product.findOne(productFilter);
+    //update product quantity
+    productToAdd.quantity-=1;
+    await Product.updateOne(productFilter, {$set:productToAdd});
+
+    const quantity:any=1;
     const price:any=productToAdd.price;
     const productName:any=productToAdd.name;
+
     let cart=await Cart.findOne({"createdBy.id":userId});
     if (cart) {
-        //cart exists for user
+    //     //cart exists for user
         const itemIndex=cart.products.findIndex(p => p.productId as unknown === productId);
         if (itemIndex > -1) {
           //product exists in the cart, update the quantity
-          let productItem = cart.products[itemIndex];
-          let oldProductQuantity:any=productItem.quantity;
-          productItem.quantity =updateQuantity(oldProductQuantity,productToAdd.quantity);;
+          let productItem:any = await cart.products[itemIndex] ;
+          productItem.quantity +=1;
           cart.products[itemIndex] = productItem;
-        //   cart.totalPrice+=productItem.price;
-        } else {
+          cart.totalPrice+=productItem.price?productItem.price:0;
+        } 
+        else {
           //product does not exists in cart, add new item
           cart.products.push({productName,quantity,price,productId});
         }
         cart = await cart.save();
         return response.status(statusCode.CREATED).send(cart);
-      } else {
+    }
+    else {
         //no cart for user, create new cart
         const user=await User.findOne({"id":userId});
         const newCart = await Cart.create({
@@ -42,7 +46,7 @@ export async function addProduct(request: express.Request, response: express.Res
           createdAt: new Date()
         });
         return response.status(statusCode.CREATED).send(newCart);
-      }
+    }
 
 }
 export async function getCart(request: express.Request, response: express.Response) {
@@ -61,28 +65,36 @@ export async function getCart(request: express.Request, response: express.Respon
 export async function removeProduct(request: express.Request, response: express.Response) {
     const userId=request.params.userId;
     const productId=request.params.productId;
-    const productToDelete=await Product.findOne({"_id":productId}) || new Product();
+    const productFilter={"_id":productId};
+    const productToDelete: any=await Product.findOne(productFilter);
     let cart=await Cart.findOne({"createdBy.id":userId});
     if(cart && productToDelete){
-        if(cart.products.length==1 && cart.products[0].quantity == 1){
-            await cart.delete();
+        productToDelete.quantity+=1;
+        await Product.updateOne(productFilter,{$set:productToDelete});
+
+        const itemIndex=cart.products.findIndex(p => p.productId as unknown == productId);
+        let productItem :any= cart.products[itemIndex];
+        productItem.quantity-=1;
+        if(productItem.quantity<=0){
+            await cart.deleteOne({"products.productId":productId});
+            if(await cart.products.length==0){
+                await cart.delete()
+                .then(()=>{
+                    return response.status(statusCode.OK).json("Cart is deleted");
+                })
+                .catch((err:any)=>{
+                    return response.status(statusCode.OK).json("Cart is deleted");
+                })
+            }
         }
-        else{
-            // await cart.deleteOne({"products.productId":productId});
-            const itemIndex=cart.products.findIndex(p => p.productId as unknown == productId);
-            let productItem = cart.products[itemIndex];
-            let oldProductQuantity:any=productItem.quantity;
-            cart.products[itemIndex].quantity =updateQuantity(oldProductQuantity, -1);
-            cart.totalPrice-=productToDelete.price;
-            cart = await cart.save();
+        else {
+            cart.products[itemIndex]=productItem;
         }
+        cart.totalPrice-=productToDelete.price;
+        cart = await cart.save();
         return response.status(statusCode.OK).json(cart);
     }
     else{
         console.log("Wrong data for product id or user id");
     }
-}
-
-function updateQuantity(oldQuantity:number, quantityToCalcualte:number){
-    return oldQuantity+quantityToCalcualte;
 }
