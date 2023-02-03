6ffd0ba99712616d44d5bc77b3f7cb0759d2f767
Salah Ashraf <salahashraf924@gmail.com>
2023-02-03 02:33:48 +0200

    add login route

--- E-Commerce Backend/src/routes/UserRoutes.ts
+++ E-Commerce Backend/src/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-const {createUser,updateUser,deleteUser}=require("../controller/UserController");
+const {login,createUser,updateUser,deleteUser}=require("../controller/UserController");
 const router = express.Router();
 
+router.post('/users/login',login)
 router.post('/users', createUser);
 router.patch('/users/:userId',updateUser);
 router.delete('/users/:userId',deleteUser);
+
 
 export default router;
