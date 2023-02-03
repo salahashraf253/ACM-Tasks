1c5b1ec94fc34b00a8b34e32b4df80e50d535881
Salah Ashraf <salahashraf924@gmail.com>
2023-02-03 02:34:17 +0200

    add function compare password

--- E-Commerce Backend/src/HelperFunctions/index.ts
+++ E-Commerce Backend/src/HelperFunctions/index.ts
@@ -1,10 +1,14 @@
 import bcrypt from "bcryptjs";
 const MAX_ID_NUMBER=10000000000;
 const MIN_ID_NUMBER=1;
+const saltRounds=10;
 
-export async function encryptPassword(unhasedPassword: string) {
-  let hashedPassword = bcrypt.hash(unhasedPassword, 10);
-  return hashedPassword;
+export async function hashPassword(unhashedPassword: string) {
+  let hashedPassword :any= bcrypt.hash(unhashedPassword,saltRounds);
+  return hashedPassword as string;
+}
+export async function comparePassword(unhashedPassword:string, hashedPassword:string) {
+  return await bcrypt.compare(unhashedPassword,hashedPassword);
 }
 export function generateRandomID() {
   const randomNumber = Math.floor(Math.random() * (MAX_ID_NUMBER - MIN_ID_NUMBER + 1) + MIN_ID_NUMBER);
