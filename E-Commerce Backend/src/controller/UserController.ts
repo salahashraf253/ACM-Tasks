1ef4c238729d8f45bf28bdaac69c4fc84c36e81b
Salah Ashraf <salahashraf924@gmail.com>
2023-02-03 02:34:37 +0200

    implement login function

--- E-Commerce Backend/src/controller/UserController.ts
+++ E-Commerce Backend/src/controller/UserController.ts
@@ -1,12 +1,27 @@
 import express from 'express';
 const User=require('../model/user').User;
-import {encryptPassword,generateRandomID}from '../HelperFunctions/index';
+import bcrypt from "bcryptjs";
+import {comparePassword,hashPassword,generateRandomID}from '../HelperFunctions/index';
 import statusCode from '../constants/StatusCode';
+
+export async function login(request: express.Request, response: express.Response) {
+    const userFound = await User.findOne({ email: request.body.email });
+    if (!userFound) {
+        return response.status(statusCode.NOT_AUTHORIZED).json("Not found email");
+    }
+    const validPassword =await bcrypt.compare(request.body.password, userFound.password);
+    // const validPassword =comparePassword(request.body.password.toString(),userFound.password);
+
+    if (!validPassword) {          
+        return response.status(statusCode.NOT_AUTHORIZED).json('Wrong Password');
+    }
+    return response.status(statusCode.OK).send("Signed In.");
+}
 
 export async function createUser(request: express.Request, response: express.Response) {
     let userToCreate=new User(request.body);
     userToCreate.id=generateRandomID();
-    userToCreate.password= await encryptPassword(userToCreate.password);
+    userToCreate.password= await hashPassword(userToCreate.password);
     userToCreate=await User.create(userToCreate);
     return response.status(statusCode.CREATED).json(`Signed Up. ${userToCreate}`);
 }
@@ -14,7 +29,7 @@ export async function updateUser(request: any, response: express.Response){
     let userId=request.params.userId;
     let userToUpdate=({
         name:request.body.name,
-        password:await encryptPassword(request.body.password),
+        password:await hashPassword(request.body.password),
         phoneNumber: request.body.phoneNumber,
         email:request.body.email,
         accountType:request.body.accountType,
