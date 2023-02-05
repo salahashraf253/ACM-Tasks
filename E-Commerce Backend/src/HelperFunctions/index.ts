1a23731f073b9ed12d76b5ba46ae01a322e54adf
Salah Ashraf <salahashraf924@gmail.com>
2023-02-05 17:32:41 +0200

    fix type of hashedPassword

--- E-Commerce Backend/src/HelperFunctions/index.ts
+++ E-Commerce Backend/src/HelperFunctions/index.ts
@@ -4,13 +4,10 @@ const MIN_ID_NUMBER=1;
 const saltRounds=10;
 
 export async function hashPassword(unhashedPassword: string) {
-  let hashedPassword :any= bcrypt.hash(unhashedPassword,saltRounds);
-  return hashedPassword as string;
+  let hashedPassword :string= (await bcrypt.hash(unhashedPassword,saltRounds)).toString();
+  return hashedPassword;
 }
 export async function comparePassword(unhashedPassword:string, hashedPassword:string) {
   return await bcrypt.compare(unhashedPassword,hashedPassword);
 }
-// export function generateRandomID() {
-//   const randomNumber = Math.floor(Math.random() * (MAX_ID_NUMBER - MIN_ID_NUMBER + 1) + MIN_ID_NUMBER);
-//   return randomNumber.toString();
-// }
+
